03056362a28808c5f81c9beca8a5958f033de367
Jimlouis09 <jimlouis900@gmail.com>
2026-07-16 17:20:17 +0800

    Add files via upload

---
+++ JLD/age.php
@@ -0,0 +1,25 @@
+<?php
+
+$age = 99;
+
+
+
+if ($age <= 12){
+    echo "Child";
+
+}
+elseif ($age <= 17){
+    echo "Teenager";
+}
+elseif ($age <= 59){
+    echo "Adult";
+}
+else{
+    echo "Senior Citizen";
+}
+
+
+
+
+
+?>
